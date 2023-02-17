fa5b45d90441f2e161e1b4e7a2f299be535a176b
dmtiriyy <dim.evtushenckoyandex.ua@gmail.com>
2023-02-17 12:49:01 +0200

    add middleware

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,19 @@
+
+export const fetchHeroes = (request) => (dispatch) => {
+    dispatch(heroesFetching());
+        request("http://localhost:3001/heroes")
+            .then(data => dispatch(heroesFetched(data)))
+            .catch(() => dispatch(heroesFetchingError()))
+}
+
+
+export const fetchFilters = (request) => (dispatch) => {
+    dispatch(filtersFetching());
+        request("http://localhost:3001/filters")
+            .then(data => dispatch(filtersFetched(data)))
+            .catch(() => dispatch(filtersFetchingError()))
+}
+
 export const heroesFetching = () => {
     return {
         type: 'HEROES_FETCHING'
@@ -36,12 +52,21 @@ export const filtersFetchingError = () => {
     }
 }
 
-export const activeFilterChanged = (filter) => {
+export const activeFilterChanged = (filter) =>  {
     return {
         type: 'ACTIVE_FILTER_CHANGED',
         payload: filter
     }
 }
+
+/* export const activeFilterChanged = (filter) => (dispatch) => {
+    setTimeout(() => {
+        dispatch({
+            type: 'ACTIVE_FILTER_CHANGED',
+            payload: filter
+        })
+    }, 1000)
+} */
 
 export const heroCreated = (hero) => {
     return {
