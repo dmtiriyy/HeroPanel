47673162e915cb65591313d12a67d7f09a28d9b1
dmtiriyy <dim.evtushenckoyandex.ua@gmail.com>
2023-03-04 22:20:32 +0200

    add changeSlide

--- src/components/heroesFilters/filtersSlice.js
+++ src/components/heroesFilters/filtersSlice.js
@@ -1,12 +1,14 @@
-import { createSlice, createAsyncThunk, createEntityAdapter } from "@reduxjs/toolkit";
+import { filtersSlice, createAsyncThunk, createEntityAdapter } from "@reduxjs/toolkit";
 import {useHttp} from '../../hooks/http.hook';
 
 const filtersAdapter = createEntityAdapter();
 
-const initialState = filtersAdapter.getInitialState({
-    filtersLoadingStatus: 'idle',
-    activeFilter: 'all'
-});
+const changeSlide = (i) => {
+    this.setState(({slide}) => ({
+        slide: slide + i
+    }))
+}
+
 
 export const fetchFilters = createAsyncThunk(
     'filters/fetchFilters',
@@ -15,84 +17,8 @@ export const fetchFilters = createAsyncThunk(
         return await request("http://localhost:3001/filters");
     }
 );
-const withSlider = (BaseComponent, getData) => {
-    return (props) => {
-        const [slide, setSlide] = useState(0);
-        const [autoplay, setAutoplay] = useState(false)
-    
-        useEffect(() => {
-            setSlide(getData());
-        }, [])
-    
-        function changeSlide(i) {
-            setSlide(slide => slide + i);
-        } 
 
-    return <BaseComponent 
-    {...props}
-    slide={slide}
-     autoplay={autoplay}
-    changeSlide= {changeSlide}
-    setAutoplay={setAutoplay}/>
-    }
-}
-const getDataFromFirstFetch = () => {return 10};
-    const getDataFromSecondFetch = () => {return 20};
-const filtersSlice = createSlice({
-    name: 'filters',
-    initialState,
-    reducers: {
-        filtersChanged: (state, action) => {
-            state.activeFilter = action.payload;
-        }
-    },
-    extraReducers: (builder) => {
-        builder
-            .addCase(fetchFilters.pending, state => {state.filtersLoadingStatus = 'loading'})
-            .addCase(fetchFilters.fulfilled, (state, action) => {
-                state.filtersLoadingStatus = 'idle';
-                filtersAdapter.setAll(state, action.payload);
-            })
-            .addCase(fetchFilters.rejected, state => {
-                state.filtersLoadingStatus = 'error';
-            })
-            .addDefaultCase(() => {})
-    }
-});
 
-  state={
-            name:'',
-            salary:''
-        }
-    
-        const {name, salary, onDelete, onToggleProp, increase, rise} = props;
-       
-
-        let classNames = "list-group-item d-flex justify-content-between";
-        if (increase) {
-            classNames += ' increase';
-        }
-        if (rise) {
-            classNames += ' like';
-        }
-    onValueChange = (e) =>{
-        this.setState({
-            [e.target.name]: e.target.value
-            
-        })
-    }
-const buttons = buttonsData.map(({name, label})=>{
-    const active = props.filter === name;
-    const clazz = active ? 'btn-light' : "btn btn-outline-light"
-    return (
-        <button type="button"
-        className={`btn ${clazz}`}
-        key = {name}
-        onClick={() => props.onFilterSelect(name)}>
-        {label}
-</button>
-    )
-})
 const {actions, reducer} = filtersSlice;
 
 export default reducer;
