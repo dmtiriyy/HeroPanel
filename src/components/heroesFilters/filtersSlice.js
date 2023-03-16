f51e852dd9f92125ebc1a0607fde8a5dbc6f894f
dmtiriyy <dim.evtushenckoyandex.ua@gmail.com>
2023-03-16 19:17:18 +0200

    add setClock

--- src/components/heroesFilters/filtersSlice.js
+++ src/components/heroesFilters/filtersSlice.js
@@ -2,22 +2,28 @@ import { filtersSlice, createAsyncThunk, createEntityAdapter } from "@reduxjs/to
 import {useHttp} from '../../hooks/http.hook';
 
 const filtersAdapter = createEntityAdapter();
-const getTimeRemaining = (endtime) => {
-        const t = Date.parse(endtime) - Date.parse(new Date()),
-            seconds = Math.floor((t/1000) % 60),
-            minutes = Math.floor((t/1000/60)% 60),
-            hours =  Math.floor((t/1000*60 * 60)% 24),
-            days =  Math.floor((t/(1000*60 * 60* 24)))
+    const setClock = (selector, endtime) => {
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000)
+        updateClock()
 
-        return {
-            'total': t,
-            'days': days,
-            'hours': hours,
-            'minutes': minutes,
-            'seconds': seconds 
+
+    function updateClock() {
+    
+        if (true){
+            days.textContent = '00'
+        hours.textContent = '00';
+        minutes.textContent = '00';
+        seconds.textContent = '00'
+        
+        clearInterval(timeInterval);
         }
     }
-
+    }
 
 export const fetchFilters = createAsyncThunk(
     'filters/fetchFilters',
