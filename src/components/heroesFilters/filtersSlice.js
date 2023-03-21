2cb5da2647c0a4a6fc1f7dc7f195e3aafaf52f74
dmtiriyy <dim.evtushenckoyandex.ua@gmail.com>
2023-03-21 18:12:56 +0200

    add FormData

--- src/components/heroesFilters/filtersSlice.js
+++ src/components/heroesFilters/filtersSlice.js
@@ -78,6 +78,36 @@ function calcScroll() {
 
             return scrollWidth;
         }
+        
+    form.forEach(item => {
+        item.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            let statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            item.appendChild(statusMessage);
+
+            const formData = new FormData(item);
+
+            if(item.getAttribute('data-calc') === 'end') {
+                for (let key in state) {
+                    formData.append(key, state[key])
+                }
+            }
+
+            postData('assets/server.php', formData)
+                .then(res => {
+                    statusMessage.textContent = message.success;
+                })
+                .catch(() => statusMessage.textContent = message.failure)
+                .finally(() => {
+                    clearInputs();
+                    setTimeout(() => {
+                        statusMessage.remove()
+                    }, 5000)
+                })
+        })
+    })
 export const fetchFilters = createAsyncThunk(
     'filters/fetchFilters',
     async () => {
